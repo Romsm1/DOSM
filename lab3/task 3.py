7bdf775c314a6916f6b52d5d6d2fbc5c9d1e6849
Romsm1 <semenkovroman06@gmail.com>
2025-05-26 09:20:23 +0900

    Update task 3.py

--- lab3/task 3.py
+++ lab3/task 3.py
@@ -1,29 +1,30 @@
 def cache(func):
-    cache_dict ={}
+    cache_dict = {}
 
     def wrapper(*args):
         if args in cache_dict:
-            print(f"\nРезультат для {args} взят из кэша.")
+            print(f"Результат для {args} взят из кэша.")
             return cache_dict[args]
 
         result = func(*args)
         cache_dict[args] = result
-        print(f"\nРезультат для {args} вычислен и сохранен в кэш.")
+        print(f"Результат для {args} вычислен и сохранен в кэш.")
         return result
 
     return wrapper
 
+
 @cache
 def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    elif n > 1:
-        return n * factorial(n - 1)
-    else:
+    if n < 0:
         return "Факториал определяется только для целых чисел."
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
 
 
-print(factorial(1))
+print(factorial(0))
 print(factorial(2))
-print(factorial(4))
-print(factorial(3))
+print(factorial(10))
+print(factorial(998))
