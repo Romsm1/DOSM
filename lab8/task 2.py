65899946999b0f46783b30eab2732da48eaa88cc
Romsm1 <semenkovroman06@gmail.com>
2025-06-02 06:32:02 +0800

    Update task 2.py

--- lab8/task 2.py
+++ lab8/task 2.py
@@ -19,8 +19,7 @@ class Student(Person):
     student_id: str
 
     def full_name(self) -> str:
-         return f"{self.first_name} {self.last_name}"
-
+        return f"{self.first_name} {self.last_name}"
 
     def get_id(self) -> str:
         return self.student_id
@@ -32,29 +31,36 @@ class Teacher(Person):
     last_name: str
     employee_id: str
     courses: list[str]
-
+
     def full_name(self) -> str:
-         return f"{self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name}"
 
     def get_id(self) -> str:
         return self.employee_id
 
 
-def people_info(people: list[Person]):
-    for person in people:
-        print(f'Полное имя: {person.full_name()}, ID: {person.get_id()}')
+def print_person(person: Person) -> str:
+    if isinstance(person, Student):
+        return f"Полное имя: {person.full_name()}, ID: {person.get_id()}"
+    else:
+        return f"Полное имя: {person.full_name()}, ID: {person.get_id()}, Курсы: {', '.join(person.courses)}"
 
 
-students = [
+list_student = [
     Student('Саша', 'Макеев', '123'),
     Student('Толик', 'Пупкин', '334')
 ]
 
-teachers = [
+list_teacher = [
     Teacher('Имя1', 'Фамилия', '42222', ["Физика", "Астрономия"]),
     Teacher('Имя2', 'Фамилия2', '1000', ["Математика", "Геометрия"])
 ]
 
-people = students + teachers
 
-people_info(people)
+print("Студенты:")
+for person in list_student:
+    print(print_person(person))
+
+print("Учителя:")
+for person in list_teacher:
+    print(print_person(person))
